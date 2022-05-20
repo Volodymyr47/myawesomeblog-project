525f89fd36d6af44038cc8580a1dc01b9020ea8a
Volodymyr47 <volodymyr.di@gmail.com>
2022-05-20 10:11:51 +0300

    Add new page Specific_post, add links to Blog, Home, AboutMe, Ukrainien jokes...

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=300, null=False)
+    date = models.DateTimeField(auto_now_add=False, null=False)
+    text = models.TextField(max_length=3000, null=True)
+    image = models.ImageField(upload_to='blog_images/')
+
+    def get_summary(self):
+        return self.text[:120]
+
+
+    def __str__(self):
+        return self.title
--- myawesomeblog/settings.py
+++ myawesomeblog/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
     'events.apps.EventsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -122,8 +123,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'myawesomeblog/static/')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 MEDIA_URL = '/awesome_media/'
